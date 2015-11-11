c240ddea1ce88520e135c0628e45b2f249346802
Ben Ripkens <bripkens.dev@gmail.com>
2015-11-11 10:41:31 +0100

    v1.3.0

--- dock.rb
+++ dock.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Dock < Formula
   homepage 'https://github.com/bripkens/dock'
-  url 'https://github.com/bripkens/dock/archive/v1.2.2.tar.gz'
-  sha1 'e953b0dda3b01bf990072179c64fe889c588fe0b'
+  url 'https://github.com/bripkens/dock/archive/v1.3.0.tar.gz'
+  sha1 '91bef0069b9251a2c75b737e0e295898a84ef57e'
 
   def install
     bin.install "dock"
