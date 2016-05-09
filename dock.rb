e29f0bd2063e4bd65dae1526cadd11a78d8c5be0
Ben Ripkens <bripkens.dev@gmail.com>
2016-05-09 20:47:46 +0200

    Switch to sha256

--- dock.rb
+++ dock.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Dock < Formula
   homepage 'https://github.com/bripkens/dock'
   url 'https://github.com/bripkens/dock/archive/v1.3.0.tar.gz'
-  sha1 '91bef0069b9251a2c75b737e0e295898a84ef57e'
+  sha256 'ac0e1b5eecd1e0e6473569b61c6bef48ffdf63a53f24ec892dba9b994ca0561b'
 
   def install
     bin.install "dock"
